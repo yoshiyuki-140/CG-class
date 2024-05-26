24eb4459931871d620825dad7b3028069e96f184
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2024-05-26 16:42:17 +0900

    update : numce.c

--- planet/numec.c
+++ planet/numec.c
@@ -2,7 +2,7 @@
 #include <GL/glut.h>
 
 static int year = 0, day = 0;
-static int star1_year = 0, star2_year = 0, star3_year = 0;
+static int star1_year = 0, star2_year = 0, star3_year = 0, numec_year = 0;
 
 void myInit(char *progname)
 {
@@ -25,37 +25,39 @@ void myInit(char *progname)
 
 void myDisplay(void)
 {
+    int evenness = 10;
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glColor3d(1.0, 1.0, 1.0);
     glPushMatrix();
 
-    // 恒星A (中心の黄色い星)
+    // 太陽 B
     glPushMatrix();
     glColor3d(1.0, 1.0, 0.0);
     glRotated((double)star1_year, 0.0, 1.0, 0.0);
     glTranslated(2.0, 0.0, 0.0);
-    glutSolidSphere(0.1, 10, 8);
+    glutSolidSphere(0.4, 10 * evenness, 8 * evenness);
 
-    // 恒星B (恒星Aの周りのオレンジの星)
+    // 太陽 C
     glPushMatrix();
     glColor3d(1.0, 0.5, 0.0);
     glRotated((double)star2_year, 0.0, 1.0, 0.0);
     glTranslated(0.5, 0.0, 0.0);
-    glutSolidSphere(0.07, 10, 8);
+    glutSolidSphere(0.3, 10 * evenness, 8 * evenness);
 
-    // 恒星C (恒星Bの周りの赤い星)
-    glColor3d(1.0, 0.0, 0.0);
-    glRotated((double)star3_year, 0.0, 1.0, 0.0);
-    glTranslated(0.3, 0.0, 0.0);
-    glutSolidSphere(0.05, 10, 8);
+    glPopMatrix();
+    glPopMatrix();
 
-    glPopMatrix(); // 恒星CのPopMatrix
-    glPopMatrix(); // 恒星BのPopMatrix
-    glPopMatrix(); // 恒星AのPopMatrix
-
-    // ナメック星 (緑色の惑星)
+    // 太陽A (中心の太陽)
     glColor3d(0.0, 1.0, 0.5);
-    glutSolidSphere(0.2, 20, 16);
+    glutSolidSphere(1.0, 20 * evenness, 16 * evenness);
+
+    // ナメック星
+    glPushMatrix();
+    glColor3d(0.0, 0.0, 1.0); // 惑星の色
+    glRotated((double)numec_year, 0.0, 1.0, 0.0);
+    glTranslated(4.0, 0.0, 0.0); // 外側の軌道に配置
+    glutSolidSphere(0.3, 10 * evenness, 8 * evenness);
+    glPopMatrix(); // 惑星のPopMatrix
 
     glPopMatrix();
     glutSwapBuffers();
@@ -69,7 +71,7 @@ void myReshape(int width, int height)
     gluPerspective(60.0, (double)width / (double)height, 0.1, 20.0);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(0.0, 3.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    gluLookAt(0.0, 6.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 }
 
 void myKeyboard(unsigned char key, int x, int y)
@@ -81,12 +83,14 @@ void myKeyboard(unsigned char key, int x, int y)
         star1_year = (star1_year + speed_of_year) % 360;
         star2_year = (star2_year + 2 * speed_of_year) % 360;
         star3_year = (star3_year + 3 * speed_of_year) % 360;
+        numec_year = (numec_year + speed_of_year) % 360;
         glutPostRedisplay();
         break;
     case 'k':
         star1_year = (star1_year - speed_of_year) % 360;
         star2_year = (star2_year - 2 * speed_of_year) % 360;
         star3_year = (star3_year - 3 * speed_of_year) % 360;
+        numec_year = (numec_year - speed_of_year) % 360;
         glutPostRedisplay();
         break;
     case 27: // ESC key
