d037d6e5f6588991660fe8a97e8e1eb76c582325
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2024-05-30 13:53:21 +0900

    add : In order not to emit light, I edited the code. I intended to do it. But this is not working well.

--- numec-star/numec.c
+++ numec-star/numec.c
@@ -1,134 +1,104 @@
 #include <stdlib.h>
 #include <GL/glut.h>
 #include <stdio.h>
-#include <SOIL/SOIL.h> // SOILライブラリを使用して画像を読み込む
 
-static int year = 0, day = 0;
 static int star1_year = 0, star2_year = 0, star3_year = 0, numec_year = 0;
-GLuint texture;
-
-void loadTexture()
-{
-    texture = SOIL_load_OGL_texture(
-        "textures/numec_texture.jpg", // テクスチャ画像のパス
-        SOIL_LOAD_AUTO,
-        SOIL_CREATE_NEW_ID,
-        SOIL_FLAG_INVERT_Y);
-
-    if (texture == 0)
-    {
-        printf("Failed to load texture: %s\n", SOIL_last_result());
-    }
-
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-}
 
 void myInit(char *progname)
 {
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
-    glutInitWindowSize(700, 700);
-    glutInitWindowPosition(0, 0);
-    glutCreateWindow(progname);
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_LIGHTING);
-    glEnable(GL_TEXTURE_2D); // テクスチャを有効にする
-    GLfloat light_position[] = {0.0, 0.0, 1.0, 1.0};
-    GLfloat white_light[] = {1.0, 1.0, 1.0, 1.0};
-    GLfloat ambient_light[] = {0.2, 0.2, 0.2, 1.0};
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, white_light);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, white_light);
-    glLightfv(GL_LIGHT0, GL_AMBIENT, ambient_light);
-    glEnable(GL_LIGHT0);
-    glClearColor(0.0, 0.0, 0.0, 0.0);
-
-    loadTexture(); // テクスチャの読み込み
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH); // ディスプレイモードを初期化
+    glutInitWindowSize(700, 700);                              // ウィンドウサイズを設定
+    glutInitWindowPosition(0, 0);                              // ウィンドウの位置を設定
+    glutCreateWindow(progname);                                // ウィンドウを作成
+    glEnable(GL_DEPTH_TEST);                                   // 深度テストを有効化
+    glEnable(GL_LIGHTING);                                     // 照明を有効化
+    GLfloat light_position[] = {0.0, 0.0, 1.0, 1.0};           // 光源の位置
+    GLfloat white_light[] = {1.0, 1.0, 1.0, 1.0};              // 白色光
+    GLfloat ambient_light[] = {0.2, 0.2, 0.2, 1.0};            // 環境光
+    // glLightfv(GL_LIGHT0, GL_POSITION, light_position);         // 光源の位置を設定
+    // glLightfv(GL_LIGHT0, GL_DIFFUSE, white_light);             // 拡散光を設定
+    // glLightfv(GL_LIGHT0, GL_SPECULAR, white_light);            // 鏡面光を設定
+    // glLightfv(GL_LIGHT0, GL_AMBIENT, ambient_light);           // 環境光を設定
+    glEnable(GL_LIGHT0);              // 光源0を有効化
+    glClearColor(0.0, 0.0, 0.0, 0.0); // 背景色を設定
 }
 
 void myDisplay(void)
 {
     int evenness = 10;
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor3d(1.0, 1.0, 1.0);
-    glPushMatrix();
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // カラーバッファとデプスバッファをクリア
+    glColor3d(1.0, 1.0, 1.0);                           // 描画色を白に設定
+    glPushMatrix();                                     // 現在の行列を保存
 
     // 太陽 B
-    glPushMatrix();
-    glColor3d(1.0, 1.0, 0.0);
-    GLfloat emissionB[] = {1.0, 1.0, 0.0, 1.0}; // 黄色の発光
-    glMaterialfv(GL_FRONT, GL_EMISSION, emissionB);
-    glRotated((double)star1_year, 0.0, 1.0, 0.0);
-    glTranslated(4.0, 0.0, 0.0);
-    glutSolidSphere(0.4, 10 * evenness, 8 * evenness);
+    glPushMatrix();                                    // 現在の行列を保存
+    glColor3d(1.0, 1.0, 0.0);                          // 描画色を黄色に設定
+    GLfloat emissionB[] = {1.0, 1.0, 0.0, 1.0};        // 発光色を設定
+    glMaterialfv(GL_FRONT, GL_EMISSION, emissionB);    // マテリアルの発光色を設定
+    glRotated((double)star1_year, 0.0, 1.0, 0.0);      // Y軸周りに回転
+    glTranslated(4.0, 0.0, 0.0);                       // 位置を移動
+    glutSolidSphere(0.4, 10 * evenness, 8 * evenness); // 球体を描画
 
     // 太陽 C
-    glPushMatrix();
-    glColor3d(1.0, 0.5, 0.0);
-    GLfloat emissionC[] = {1.0, 0.5, 0.0, 1.0}; // オレンジ色の発光
-    glMaterialfv(GL_FRONT, GL_EMISSION, emissionC);
-    glRotated((double)star2_year, 0.0, 1.0, 0.0);
-    glTranslated(1.0, 0.0, 0.0);
-    glutSolidSphere(0.3, 10 * evenness, 8 * evenness);
-
-    glPopMatrix();
-    glPopMatrix();
+    glPushMatrix();                                    // 現在の行列を保存
+    glColor3d(1.0, 0.5, 0.0);                          // 描画色をオレンジに設定
+    GLfloat emissionC[] = {1.0, 0.5, 0.0, 1.0};        // 発光色を設定
+    glMaterialfv(GL_FRONT, GL_EMISSION, emissionC);    // マテリアルの発光色を設定
+    glRotated((double)star2_year, 0.0, 1.0, 0.0);      // Y軸周りに回転
+    glTranslated(1.0, 0.0, 0.0);                       // 位置を移動
+    glutSolidSphere(0.3, 10 * evenness, 8 * evenness); // 球体を描画
+    glPopMatrix();                                     // 保存した行列を復元
+    glPopMatrix();                                     // 保存した行列を復元
 
     // 太陽A (中心の太陽)
-    glColor3d(0.0, 1.0, 0.5);
-    GLfloat emissionA[] = {1.0, 0.5, 0.0, 1.0}; // 黄色の発行
-    glMaterialfv(GL_FRONT, GL_EMISSION, emissionA);
-    glutSolidSphere(1.0, 20 * evenness, 16 * evenness);
+    glColor3d(0.0, 1.0, 0.5);                           // 描画色をシアンに設定
+    GLfloat emissionA[] = {1.0, 0.5, 0.0, 1.0};         // 発光色を設定
+    glMaterialfv(GL_FRONT, GL_EMISSION, emissionA);     // マテリアルの発光色を設定
+    glutSolidSphere(1.0, 20 * evenness, 16 * evenness); // 大きな球体を描画
 
-    // ナメック星
-    glPushMatrix();
-    glBindTexture(GL_TEXTURE_2D, texture); // テクスチャをバインド
-    glRotated((double)numec_year, 0.0, 1.0, 0.0);
-    glTranslated(7.0, 0.0, 0.0); // 外側の軌道に配置
-    // GLfloat emissionNumec[] = {0.0, 0.0, 0.0, 1.0}; // 無発光
-    // glBindTexture(GL_TEXTURE_2D, 0); // テクスチャのバインド解除
-    glMaterialfv(GL_FRONT, GL_EMISSION, emissionNumec);
-    glutSolidSphere(0.3, 10 * evenness, 8 * evenness);
-    glPopMatrix(); // 惑星のPopMatrix
+    // ナメック星（発光なし）
+    glPushMatrix();                                    // 現在の行列を保存
+    glColor3d(0.0, 0.5, 1.0);                          // 描画色をブルーグリーンに設定
+    glRotated((double)numec_year, 0.0, 1.0, 0.0);      // Y軸周りに回転
+    glTranslated(7.0, 0.0, 0.0);                       // 位置を移動
+    glutSolidSphere(0.3, 10 * evenness, 8 * evenness); // 小さな球体を描画
+    glPopMatrix();                                     // 保存した行列を復元
 
-    glPopMatrix();
-    glutSwapBuffers();
+    glPopMatrix();     // 保存した行列を復元
+    glutSwapBuffers(); // バッファを交換して画面に表示
 }
 
 void myReshape(int width, int height)
 {
-    glViewport(0, 0, width, height);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(60.0, (double)width / (double)height, 0.1, 20.0);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    gluLookAt(0.0, 6.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    glViewport(0, 0, width, height);                                 // ビューポートを設定
+    glMatrixMode(GL_PROJECTION);                                     // 投影行列を選択
+    glLoadIdentity();                                                // 投影行列を単位行列にリセット
+    gluPerspective(60.0, (double)width / (double)height, 0.1, 20.0); // 透視投影を設定
+    glMatrixMode(GL_MODELVIEW);                                      // モデルビュー行列を選択
+    glLoadIdentity();                                                // モデルビュー行列を単位行列にリセット
+    gluLookAt(0.0, 6.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);         // 視点を設定
 }
 
 void myKeyboard(unsigned char key, int x, int y)
 {
-    int speed_of_year = 1;
+    int speed_of_year = 1; // 回転速度
     switch (key)
     {
-    case 'j':
-        star1_year = (star1_year + speed_of_year) % 360;
-        star2_year = (star2_year + 2 * speed_of_year) % 360;
-        star3_year = (star3_year + 3 * speed_of_year) % 360;
+    case 'j': // 'j'キーで前進
+        star1_year = (star1_year + 2 * speed_of_year) % 360;
+        star2_year = (star2_year + 3 * speed_of_year) % 360;
+        star3_year = (star3_year + 4 * speed_of_year) % 360;
         numec_year = (numec_year + speed_of_year) % 360;
-        glutPostRedisplay();
+        glutPostRedisplay(); // 再描画
         break;
-    case 'k':
-        star1_year = (star1_year - speed_of_year) % 360;
-        star2_year = (star2_year - 2 * speed_of_year) % 360;
-        star3_year = (star3_year - 3 * speed_of_year) % 360;
+    case 'k': // 'k'キーで後退
+        star1_year = (star1_year - 2 * speed_of_year) % 360;
+        star2_year = (star2_year - 3 * speed_of_year) % 360;
+        star3_year = (star3_year - 4 * speed_of_year) % 360;
         numec_year = (numec_year - speed_of_year) % 360;
-        glutPostRedisplay();
+        glutPostRedisplay(); // 再描画
         break;
-    case 27: // ESC key
+    case 27: // ESCキーで終了
         exit(0);
         break;
     default:
@@ -138,11 +108,11 @@ void myKeyboard(unsigned char key, int x, int y)
 
 int main(int argc, char **argv)
 {
-    glutInit(&argc, argv);
-    myInit(argv[0]);
-    glutKeyboardFunc(myKeyboard);
-    glutReshapeFunc(myReshape);
-    glutDisplayFunc(myDisplay);
-    glutMainLoop();
+    glutInit(&argc, argv);        // GLUTを初期化
+    myInit(argv[0]);              // 初期設定関数を呼び出し
+    glutKeyboardFunc(myKeyboard); // キーボード関数を設定
+    glutReshapeFunc(myReshape);   // リサイズ関数を設定
+    glutDisplayFunc(myDisplay);   // ディスプレイ関数を設定
+    glutMainLoop();               // イベント処理ループに入る
     return 0;
 }
