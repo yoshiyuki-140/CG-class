4c909428dfb0dc1c693fe0600827fa0c0fe4c478
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2024-05-31 15:01:18 +0900

    update : マッピングに失敗したくさい

--- firstAssignment/numec-star/numec.c
+++ firstAssignment/numec-star/numec.c
@@ -4,8 +4,10 @@
 #include "stb_image.h"
 #include <ctype.h>
 
-#define imageWidth 700
-#define imageHeight 402
+// #define imageWidth 700  // テクスチャ画像の縦幅
+// #define imageHeight 402 // テクスチャ画像の横幅
+#define imageWidth 256  // テクスチャ画像の縦幅
+#define imageHeight 256 // テクスチャ画像の横幅
 
 static int star1_year = 0, star2_year = 0, star3_year = 0, numec_year = 0;
 // エメラルドのマテリアル
@@ -13,13 +15,15 @@ GLfloat mtrl_ambient[] = {0.0215, 0.1745, 0.0215, 1.0}, mtrl_diffuse[] = {0.0756
 
 unsigned char texImage[imageHeight][imageWidth][3];
 
+// 画像読み取り用の関数
 void readPPMImage(char *filename)
 {
   FILE *fp;
   int ch, i;
 
-  if ((fp = fopen(filename, "r")) == NULL)
+  if ((fp = fopen(filename, "r")) == NULL) // 読み取りモードで指定されたPPMファイルを読み取り
   {
+    // ファイル読み取りが失敗したときの処理
     fprintf(stderr, "Cannot open ppm file %s\n", filename);
     exit(1);
   }
@@ -50,11 +54,11 @@ void setUpTexture(void)
 {
   readPPMImage("./textures/numec_texture.ppm");
   glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, imageWidth, imageHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, texImage);
+  glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+  glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexImage2D(GL_TEXTURE_3D, 0, GL_RGB, imageWidth, imageHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, texImage);
 }
 
 void myInit(char *progname)
@@ -68,12 +72,12 @@ void myInit(char *progname)
   GLfloat light_position[] = {0.0, 0.0, 1.0, 1.0};           // 光源の位置
   GLfloat white_light[] = {1.0, 1.0, 1.0, 1.0};              // 白色光
   GLfloat ambient_light[] = {0.2, 0.2, 0.2, 1.0};            // 環境光
-  // glLightfv(GL_LIGHT0, GL_POSITION, light_position);         // 光源の位置を設定
-  // glLightfv(GL_LIGHT0, GL_DIFFUSE, white_light);             // 拡散光を設定
-  // glLightfv(GL_LIGHT0, GL_SPECULAR, white_light);            // 鏡面光を設定
-  // glLightfv(GL_LIGHT0, GL_AMBIENT, ambient_light);           // 環境光を設定
-  glEnable(GL_LIGHT0);              // 光源0を有効化
-  glClearColor(0.0, 0.0, 0.0, 0.0); // 背景色を設定
+  glLightfv(GL_LIGHT0, GL_POSITION, light_position);         // 光源の位置を設定
+  glLightfv(GL_LIGHT0, GL_DIFFUSE, white_light);             // 拡散光を設定
+  glLightfv(GL_LIGHT0, GL_SPECULAR, white_light);            // 鏡面光を設定
+  glLightfv(GL_LIGHT0, GL_AMBIENT, ambient_light);           // 環境光を設定
+  glEnable(GL_LIGHT0);                                       // 光源0を有効化
+  glClearColor(0.0, 0.0, 0.0, 0.0);                          // 背景色を設定
 }
 
 void myDisplay(void)
@@ -83,8 +87,9 @@ void myDisplay(void)
   glColor3d(1.0, 1.0, 1.0);                           // 描画色を白に設定
   glPushMatrix();                                     // 現在の行列を保存
 
-  // ナメック星（発光なし）
-  glPushMatrix();           // 現在の行列を保存
+  // ナメック星 : こいつは発光しない
+  glPushMatrix(); // 現在の行列を保存
+  glEnable(GL_TEXTURE_3D);
   glColor3d(0.0, 0.5, 1.0); // 描画色をブルーグリーンに設定
   // これが発光の設定
   // START
@@ -94,15 +99,16 @@ void myDisplay(void)
 
   // ナメック星の材質の設定
   // GLfloat mtrl_ambient[] = {0.0215, 0.1745, 0.0215, 1.0}, mtrl_diffuse[] = {0.07568, 0.61424, 0.07568, 1.0}, mtrl_specular[] = {0.5, 0.5, 0.5, 1.0}, mtrl_shiniess[] = {128.0};
-  glMaterialfv(GL_FRONT, GL_AMBIENT, mtrl_ambient);    // マテリアルの環境光を設定
-  glMaterialfv(GL_FRONT, GL_DIFFUSE, mtrl_diffuse);    // マテリアルの拡散光を設定
-  glMaterialfv(GL_FRONT, GL_SPECULAR, mtrl_specular);  // マテリアルの鏡面光を設定
-  glMaterialfv(GL_FRONT, GL_SHININESS, mtrl_shiniess); // マテリアルの鏡面反射指数を設定
+  // glMaterialfv(GL_FRONT, GL_AMBIENT, mtrl_ambient);    // マテリアルの環境光を設定
+  // glMaterialfv(GL_FRONT, GL_DIFFUSE, mtrl_diffuse);    // マテリアルの拡散光を設定
+  // glMaterialfv(GL_FRONT, GL_SPECULAR, mtrl_specular);  // マテリアルの鏡面光を設定
+  // glMaterialfv(GL_FRONT, GL_SHININESS, mtrl_shiniess); // マテリアルの鏡面反射指数を設定
 
   glRotated((double)numec_year, 0.0, 1.0, 0.0);      // Y軸周りに回転
   glTranslated(7.0, 0.0, 0.0);                       // 位置を移動
   glutSolidSphere(0.3, 10 * evenness, 8 * evenness); // 小さな球体を描画
-  glPopMatrix();                                     // 保存した行列を復元
+  glDisable(GL_TEXTURE_3D);
+  glPopMatrix(); // 保存した行列を復元
 
   // 太陽 B
   glPushMatrix();                                    // 現在の行列を保存
@@ -147,7 +153,7 @@ void myReshape(int width, int height)
 
 void myIdle(void)
 {
-  int speed_of_year = 2; // 回転速度
+  int speed_of_year = 1; // 回転速度
   star1_year = (star1_year + 2 * speed_of_year) % 360;
   star2_year = (star2_year + 3 * speed_of_year) % 360;
   star3_year = (star3_year + 4 * speed_of_year) % 360;
@@ -184,10 +190,10 @@ void myKeyboard(unsigned char key, int x, int y)
 
 int main(int argc, char **argv)
 {
-  glutInit(&argc, argv);        // GLUTを初期化
-  myInit(argv[0]);              // 初期設定関数を呼び出し
-  glutKeyboardFunc(myKeyboard); // キーボード関数を設定
-  glutReshapeFunc(myReshape);   // リサイズ関数を設定
+  glutInit(&argc, argv); // GLUTを初期化
+  myInit(argv[0]);       // 初期設定関数を呼び出し
+  // setUpTexture();               // テクスチャのセットアップ
+  glutKeyboardFunc(myKeyboard); // キーボード関数を設定 glutReshapeFunc(myReshape);   // リサイズ関数を設定
   glutIdleFunc(myIdle);         // アイドル関数を設定
   glutDisplayFunc(myDisplay);   // ディスプレイ関数を設定
   glutMainLoop();               // イベント処理ループに入る
